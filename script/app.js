be5a0c898e9eb3fdb8acda4863fc147aca0119d7
Sergey Koternyak <sergeykoternyak@gmail.com>
2016-08-01 00:31:10 +0300

    Update files

--- script/app.js
+++ script/app.js
@@ -1,5 +1,10 @@
+import angular from 'angular';
+import base64Upload from 'angular-base64-upload';
+import datepicker from 'angularjs-datepicker';
 import MainController from './mainController.js';
 import ContactController from './contactController.js';
+import '../styles/style.less';
+
 
 angular.module('phonebookApp', ['naif.base64','720kb.datepicker'])
 	.controller('MainController', MainController)
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: "./script/app.js",
+
 	resolve: {
 		modulesDirectories: [
-			"./mainController.js",
-			"./contactController.js"
+			"./node_modules/"
 		]
 	},
+
 	output: {
 		filename: "script.js"
 	},
+
 	module: {
 		loaders: [
 			{
 				test: /\.js$/,
 				loader: "babel-loader",
 				query: {
-					presets: ['es2015']
+					presets: ['es2015'],
+					compact: false
 				}
+			},
+			{
+				test: /\.(png|jpg|woff)$/,
+				loader: 'file?name=[path][name].[ext]'
+			},
+			{
+				test: /\.less$/,
+				loader: ExtractTextPlugin.extract('style-loader', 'css!less')
 			}
 		]
 	},
-	// watch: true
+	plugins: [
+		new ExtractTextPlugin('style.css')
+	]
 };
